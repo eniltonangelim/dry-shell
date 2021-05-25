7c6d46b51e27f32696c2ccd7e7c9183bfc7c0f6f
Angelim <enilton.angelim@gmail.com>
2021-05-24 21:40:12 -0300

    doc(example) find symbolic links

---
+++ examples/findLinks.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+export LIB_HOME="/root/fslib"
+export log_file=/tmp/log.txt
+
+source is/index.sh
+source screen/index.sh
+
+is.file.exists? $log_file && rm $log_file
+
+clear
+
+screen.box.dialog 1 1 'find symbolic links'
+
+function runnable() {
+    function process() {
+        echo "$1 is link of `readlink -f $1`" >> $log_file
+    }
+
+    while read line; do is.file.link? $link && process $line; done <<< `find -L ${1:-/etc}` #-L symbolic links
+}
+
+runnable /etc
